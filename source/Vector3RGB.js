e563b3ae20f79c4512dd36d2dc53bef4622dab71
chkt <ck@christoph-kettelhoit.de>
2016-11-07 18:32:54 +0100

    Fix .CSS() when using hsl formated strings

--- source/Vector3RGB.js
+++ source/Vector3RGB.js
@@ -3,30 +3,12 @@ import Vector3 from 'xyzw/es5/Vector3';
 
 import * as css from './css';
 import * as hsl from './hsl';
-
-
-
-const ONE_DIV_TWOFIFTYSIX = 1.0 / 256.0;
-
-
-
-/**
- * Returns the rgb8 value representing f
- * @param {number} f - The floating point value
- * @returns {int}
- */
-function floatToInt(f) {
-	return Math.clamp(Math.floor(f * 256.0), 0, 255);
-}
-
-/**
- * Returns the floating point value representing i
- * @param {int} i - The rgb8 value
- * @returns {number}
- */
-function intToFloat(i) {
-	return i < 255 ? i * ONE_DIV_TWOFIFTYSIX : 1.0;
-}
+import {
+	floatToInt,
+	intToFloat,
+	intIntIntToFloat,
+	degPctPctToFloat
+} from './convertRGB';
 
 
 
@@ -58,29 +40,24 @@ export default class Vector3RGB extends Vector3 {
 	 */
 	static CSS(string, matte = new Vector3([1.0, 1.0, 1.0]), target = undefined) {
 		const { type, components } = css.parse(string);
-		const a = components.pop(), rgb = type === 'rgb' ? components : hsl.hslToRgb(components);
+		const a = components.pop();
+		const rgb = type === 'rgb' ? intIntIntToFloat(components) : hsl.hslToRgb(degPctPctToFloat(components));
 		const mn = matte.n;
 
-		const n = [
-			Math.mix(mn[0], intToFloat(rgb[0]), a),
-			Math.mix(mn[1], intToFloat(rgb[1]), a),
-			Math.mix(mn[2], intToFloat(rgb[2]), a)
-		];
-
-		return this.Define(n, target);
+		return this.Define([
+			Math.mix(mn[0], rgb[0], a),
+			Math.mix(mn[1], rgb[1], a),
+			Math.mix(mn[2], rgb[2], a)
+		], target);
 	}
 
 	/**
-	 * Returns an instance representing rrggbb hex encoded i
+	 * Returns an instance representing 0xrrggbb hex encoded i
 	 * @param {int} i - The bit encoded Int
 	 * @param {Vector3} [target] - The target instance
 	 * @returns {Vector3RGB}
-	 * @throws {TypeError} if i is not an int
-	 * @throws {TypeError} if scale is not a number
 	 */
 	static Int(i, target = undefined) {
-		if (!Number.isSafeInteger(i)) throw new TypeError();
-
 		return this.Define([
 			intToFloat(i >> 16 & 0xff),
 			intToFloat(i >> 8 & 0xff),
@@ -151,7 +128,7 @@ export default class Vector3RGB extends Vector3 {
 
 
 	/**
-	 * Returns a rrggbb hex encoded integer representation of the instance
+	 * Returns a 0xrrggbb hex encoded integer representation of the instance
 	 * @returns {int}
 	 */
 	toInt() {
