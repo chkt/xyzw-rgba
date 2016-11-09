e31a9274dbb2edf3ccb58baffa99de7bea85076f
chkt <ck@christoph-kettelhoit.de>
2016-11-09 13:13:53 +0100

    Refactor HSLA

--- source/HSLA.js
+++ source/HSLA.js
@@ -1,13 +1,11 @@
 import Math from 'xyzw/es5/Math';
 import Vector3 from 'xyzw/es5/Vector3';
-import Vector4 from 'xyzw/es5/Vector4';
+import Vector3RGB from './Vector3RGB';
+import Vector4RGBA from './Vector4RGBA';
 
-
-
-const RAD_TO_DEG = 180.0 / Math.PI;
-
-const ONE_THIRD_PI = 1.0 / 3.0 * Math.PI;
-const PI_DIV_THREE = 3.0 / Math.PI;
+import * as hsl from './hsl';
+import * as css from './css';
+import { floatToDegPctPct } from './convertRGB';
 
 
 
@@ -18,11 +16,10 @@ export default class HSLA {
 
 	/**
 	 * Returns a defined instance
-	 * @constructor
-	 * @param {Number} h - The hue in radians
-	 * @param {Number} s - The saturation
-	 * @param {Number} l - The luminosity
-	 * @param {Number} a - The alpha
+	 * @param {number} h - The hue in radians
+	 * @param {number} s - The saturation
+	 * @param {number} l - The luminosity
+	 * @param {number} a - The alpha
 	 * @param {HSLA} [target] - The target instance
 	 * @returns {HSLA}
 	 */
@@ -30,47 +27,31 @@ export default class HSLA {
 		if (target === undefined) target = new this(h, s, l, a);
 		else this.call(target, h, s, l, a);
 
-		return this;
+		return target;
 	}
 
 	/**
 	 * Returns an instance representing v
-	 * @constructor
 	 * @param {Vector4} v - The source rgba vector
 	 * @param {HSLA} [target] - The target instance
 	 * @returns {HSLA}
 	 */
 	static RGBA(v, target) {
-		const r = v.n[0], g = v.n[1], b = v.n[2];
-
-		const max = Math.max(r, g, b);
-		const min = Math.min(r, g, b);
-
-		let h, s, l, c = max - min;
-
-		if      (c === 0.0) h = 0.0;
-		else if (min === r) h = (g - b) / c % 6.0;
-		else if (min === g) h = (b - r) / c + 2.0;
-		else                h = (r - g) / c + 4.0;
-
-		h *= ONE_THIRD_PI;
-		l = 0.5 * (max + min);
-
-		if (c === 0.0) s = 0.0;
-		else s = c / (1.0 - Math.abs(2.0 * l - 1.0));
+		const [h, s, l] = hsl.rgbToHsl(v.n.slice(0, 3));
 
 		return this.Define(h, s, l, v.n[3], target);
 	}
 
 	/**
 	 * Returns an instance representing v
-	 * @constructor
 	 * @param {Vector3} v - The source rgb vector
 	 * @param {HSLA} [target] - The target instance
 	 * @returns {HSLA}
 	 */
 	static RGB(v, target) {
-		return HSLA.RGBA(Vector4.Vector3(v), target);
+		const [h, s, l] = hsl.rgbToHsl(v.n);
+
+		return this.Define(h, s, l, 1.0, target);
 	}
 
 
@@ -89,7 +70,7 @@ export default class HSLA {
 	 * Returns true if a == b, false otherwise
 	 * @param {HSLA} a
 	 * @param {HSLA} b
-	 * @returns {Boolean}
+	 * @returns {boolean}
 	 */
 	static isEQ(a, b) {
 		return a === b || a.h === b.h && a.s === b.s && a.l === b.l && a.a === b.a;
@@ -99,10 +80,10 @@ export default class HSLA {
 
 	/**
 	 * Creates a new instance
-	 * @param {Float} h - The hue in radians
-	 * @param {Float} s - The saturation
-	 * @param {Float} l - The luminosity
-	 * @param {Float} a - The alpha
+	 * @param {number} h - The hue in radians
+	 * @param {number} s - The saturation
+	 * @param {number} l - The luminosity
+	 * @param {number} a - The alpha
 	 */
 	constructor(h, s, l, a) {
 		/**
@@ -130,10 +111,10 @@ export default class HSLA {
 
 	/**
 	 * Redefines the instance
-	 * @param {Float} h - The hue in radians
-	 * @param {Float} s - The saturation
-	 * @param {Float} l - The luminosity
-	 * @param {Float} a - The alpha
+	 * @param {number} h - The hue in radians
+	 * @param {number} s - The saturation
+	 * @param {number} l - The luminosity
+	 * @param {number} a - The alpha
 	 * @returns {HSLA}
 	 */
 	define(h, s, l, a) {
@@ -145,10 +126,10 @@ export default class HSLA {
 
 	/**
 	 * The chroma
-	 * @type Float
+	 * @type {number}
 	 */
 	get chroma() {
-		return (1.0 - Math.abs(2.0 * this.l - 1.0)) * this.s;
+		return hsl.chroma(this.s, this.l);
 	}
 
 
@@ -173,24 +154,13 @@ export default class HSLA {
 	 * @returns {Vector4}
 	 */
 	toRGBA(target) {
-		const c = this.chroma;
-		const h = this.h * PI_DIV_THREE;
-		const x = c * (1.0 - Math.abs(h % 2.0 - 1.0));
+		const rgb = hsl.hslToRgb([this.h, this.s, this.l]);
 
-		let r, g, b;
+		if (target === undefined) target = new Vector4RGBA();
 
-		if      (h >= 0.0 && h < 1.0) r = c, g = x, b = 0.0;
-		else if (h >= 1.0 && h < 2.0) r = x, g = c, b = 0.0;
-		else if (h >= 2.0 && h < 3.0) r = 0.0, g = c, b = x;
-		else if (h >= 3.0 && h < 4.0) r = 0.0, g = x, b = c;
-		else if (h >= 4.0 && h < 5.0) r = x, g = 0.0, b = c;
-		else                          r = c, g = 0.0, b = x;
+		target.n = [...rgb, this.a];
 
-		const min = this.l - 0.5 * c;
-		const n = [r + min, g + min, b + min, this.a];
-
-		if (target === undefined) return new Vector4(n);
-		else return target.define(n);
+		return target;
 	}
 
 	/**
@@ -199,50 +169,39 @@ export default class HSLA {
 	 * @param {Vector3} [target] - The target vector
 	 * @returns {Vector3}
 	 */
-	toRGB(matte = new Vector3(), target) {
-		const rgba = this.toRGBA(), a = rgba.n[3];
+	toRGB(matte = new Vector3([1.0, 1.0, 1.0]), target = undefined) {
+		const rgb = hsl.hslToRgb([this.h, this.s, this.l]), a = this.a;
+		const mn = matte.n;
 
-		const n = [
-			Math.mix(matte.n[0], rgba.n[0], a),
-			Math.mix(matte.n[1], rgba.n[1], a),
-			Math.mix(matte.n[2], rgba.n[2], a)
+		if (target === undefined) target = new Vector3RGB();
+
+		target.n = [
+			Math.mix(mn[0], rgb[0], a),
+			Math.mix(mn[1], rgb[1], a),
+			Math.mix(mn[2], rgb[2], a)
 		];
 
-		if (target === undefined) return new Vector3(n);
-		else return target.define(n);
+		return target;
 	}
 
 	/**
 	 * Returns a css-formated hsl or hsla representation of the instance
-	 * @returns {String}
+	 * @returns {string}
 	 */
 	toCSS() {
-		const hsl = `${
-				Math.round(this.h * RAD_TO_DEG)
-			},${
-				Math.round(this.s * 100.0)
-			}%,${
-				Math.round(this.l * 100.0)
-			}%`;
+		const hsl = floatToDegPctPct([this.h, this.s, this.l]);
 
-		if (this.a === 1.0) return `hsl(${ hsl })`;
-		else return `hsla(${ hsl },${ this.a.toFixed(4) })`;
+		return css.stringify({
+			type : 'hsl',
+			components : [...hsl, this.a]
+		});
 	}
 
 	/**
 	 * Returns a string representation of the instance
-	 * @param {Uint} [digits=3] - The decimal digits
-	 * @returns {String}
+	 * @returns {string}
 	 */
-	toString(digits = 3) {
-		return `[HSLA](${
-				this.h.toFixed(digits)
-			},${
-				this.s.toFixed(digits)
-			},${
-				this.l.toFixed(digits)
-			},${
-				this.a.toFixed(digits)
-			})`;
+	toString() {
+		return this.toCSS();
 	}
 }
